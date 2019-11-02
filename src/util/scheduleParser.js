a0005a44d415944ee807b1d132ba190aedb092e7
unknown <arranwoodruff@gmail.com>
2019-11-01 18:24:58 -0600

    pulling employees from w2w, sid hardcoded

--- src/util/scheduleParser.js
+++ src/util/scheduleParser.js
@@ -1,48 +1,93 @@
-const fs = require("fs");
+const axios = require("axios");
 const parser = require("node-html-parser");
 const _ = require("lodash");
 
-fs.readFile("dailySchedule.html", async (err, data) => {
-  const root = parser.parse(data.toString());
+module.exports = {
+  getSched: async function() {
+    const schedule = [];
 
-  const sftstart = root.querySelectorAll(".sftstart");
-  const sftend = root.querySelectorAll(".sftend");
-  const sftpos = root.querySelectorAll(".sftpos");
+    const res = await axios.get(
+      "https://www6.whentowork.com/cgi-bin/w2wFF.dll/empshiftlist.htm?SID=181268317041A4&UTF8=Y&date=11/01/2019"
+    );
 
-  // Ensure each list is equal length
-  if (sftpos.length != sftstart.length || sftpos.length != sftend.length) {
-    throw new Error("Incoming Shift Data Error");
-  }
+    const root = parser.parse(res.data);
 
-  const startTimes = [];
-  const employees = [];
+    const sftstart = root.querySelectorAll(".sftstart");
+    const sftend = root.querySelectorAll(".sftend");
+    const sftpos = root.querySelectorAll(".sftpos");
 
-  for (let i = 0; i < sftpos.length; i++) {
-    const name = sftpos[i].childNodes[0].rawText.trim();
-    const start = sftstart[i].childNodes[0].rawText.trim();
-    const end = sftend[i].childNodes[0].rawText.trim();
+    // Ensure each list is equal length
+    if (sftpos.length != sftstart.length || sftpos.length != sftend.length) {
+      throw new Error("Incoming Shift Data Error");
+    }
 
-    if (!startTimes.includes(start)) startTimes.push(start);
+    const startTimes = [];
+    const employees = [];
 
-    const employee = {
-      name,
-      start,
-      end
-    };
+    for (let i = 0; i < sftpos.length; i++) {
+      const name = sftpos[i].childNodes[0].rawText.trim();
+      const start = sftstart[i].childNodes[0].rawText.trim();
+      const end = sftend[i].childNodes[0].rawText.trim();
 
-    if (employee.name) employees.push(employee);
-  }
+      if (!startTimes.includes(start)) startTimes.push(start);
 
-  const schedule = [];
+      const employee = {
+        name,
+        start,
+        end
+      };
 
-  startTimes.forEach(start => {
-    schedule.push({
-      start,
-      employees: _.filter(employees, { start: start })
+      if (employee.name) employees.push(employee);
+    }
+
+    startTimes.forEach(start => {
+      schedule.push({
+        start,
+        employees: _.filter(employees, { start: start })
+      });
     });
-  });
 
-  schedule.forEach(start => {
-    console.log(start.start, start.employees);
-  });
-});
+    // console.log(schedule);
+
+    return schedule;
+  }
+};
+
+// fs.readFile("dailySchedule.html", async (err, data) => {
+//   const root = parser.parse(data.toString());
+
+//   const sftstart = root.querySelectorAll(".sftstart");
+//   const sftend = root.querySelectorAll(".sftend");
+//   const sftpos = root.querySelectorAll(".sftpos");
+
+//   // Ensure each list is equal length
+//   if (sftpos.length != sftstart.length || sftpos.length != sftend.length) {
+//     throw new Error("Incoming Shift Data Error");
+//   }
+
+//   const startTimes = [];
+//   const employees = [];
+
+//   for (let i = 0; i < sftpos.length; i++) {
+//     const name = sftpos[i].childNodes[0].rawText.trim();
+//     const start = sftstart[i].childNodes[0].rawText.trim();
+//     const end = sftend[i].childNodes[0].rawText.trim();
+
+//     if (!startTimes.includes(start)) startTimes.push(start);
+
+//     const employee = {
+//       name,
+//       start,
+//       end
+//     };
+
+//     if (employee.name) employees.push(employee);
+//   }
+
+//   startTimes.forEach(start => {
+//     schedule.push({
+//       start,
+//       employees: _.filter(employees, { start: start })
+//     });
+//   });
+// });
